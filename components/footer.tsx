22fd54880db764e3128eeaf1b5c2bcee3cbc395c
RIFKIRH <rifkirahmatunh@gmail.com>
2025-05-13 22:32:42 +0700

    hero

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,10 +3,9 @@ import Image from "next/image";
 
 const Footer = () => {
   return (
-    <footer className='bg-gray-900'>
-      <div className='max-w-screen-xl mx-auto px-4 w-full py-10 md:py-16'>
+    <footer className="bg-gray-900">
+      <div className="max-w-screen-xl mx-auto px-4 w-full py-10 md:py-16">
         <div className="grid md:grid-cols-3 gap-7">
-          
           {/* Kolom 1: Logo dan Deskripsi */}
           <div>
             <Link href="/" className="mb-10 block">
@@ -23,27 +22,47 @@ const Footer = () => {
             <div>
               <h4 className="mb-8 text-xl font-semibold text-white">Links</h4>
               <ul className="list-item space-y-5 text-gray-400">
-                <li><Link href="/">Home</Link></li>
-                <li><Link href="/about">About</Link></li>
-                <li><Link href="/room">Room</Link></li>
-                <li><Link href="/contact">Contact Us</Link></li>
+                <li>
+                  <Link href="/">Home</Link>
+                </li>
+                <li>
+                  <Link href="/about">About</Link>
+                </li>
+                <li>
+                  <Link href="/room">Room</Link>
+                </li>
+                <li>
+                  <Link href="/contact">Contact Us</Link>
+                </li>
               </ul>
             </div>
             <div>
               <h4 className="mb-8 text-xl font-semibold text-white">Legal</h4>
               <ul className="list-item space-y-5 text-gray-400">
-                <li><Link href="#">Legal</Link></li>
-                <li><Link href="#">Term & Conditions</Link></li>
-                <li><Link href="#">Payment Methods</Link></li>
-                <li><Link href="#">Privacy Policy</Link></li>
+                <li>
+                  <Link href="#">Legal</Link>
+                </li>
+                <li>
+                  <Link href="#">Term & Conditions</Link>
+                </li>
+                <li>
+                  <Link href="#">Payment Methods</Link>
+                </li>
+                <li>
+                  <Link href="#">Privacy Policy</Link>
+                </li>
               </ul>
             </div>
           </div>
 
           {/* Kolom 3: Newsletter */}
           <div>
-            <h4 className="mb-8 text-xl font-semibold text-white">Newsletter</h4>
-            <p className="text-gray-400">Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+            <h4 className="mb-8 text-xl font-semibold text-white">
+              Newsletter
+            </h4>
+            <p className="text-gray-400">
+              Lorem ipsum dolor sit amet consectetur adipisicing.
+            </p>
             <form action="" className="mt-5">
               <div className="mb-5">
                 <input
@@ -53,23 +72,20 @@ const Footer = () => {
                   placeholder="johndoe@gmail.com"
                 />
               </div>
-              <button
-                className="bg-orange-400 p-3 font-bold text-white w-full text-center rounded-sm hover:bg-orange-500"
-              >
+              <button className="bg-orange-400 p-3 font-bold text-white w-full text-center rounded-sm hover:bg-orange-500">
                 Subscribe
               </button>
             </form>
           </div>
-
         </div>
       </div>
 
       {/* Copyright */}
-      <div className='max-w-screen-xl mx-auto px-4 border-t border-gray-500 py-8 text-center text-base text-gray-500'>
+      <div className="max-w-screen-xl mx-auto px-4 border-t border-gray-500 py-8 text-center text-base text-gray-500">
         &copy; Copyright 2025 | Rifki RH | All Right Reserved
       </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
